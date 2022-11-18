5f58854ba581ce71d3e823a8dad8e40265ef4497
mukeshsingh1234 <101175123+mukeshsingh1234@users.noreply.github.com>
2022-11-18 11:07:13 +0530

    Create paint.py

---
+++ paint.py
@@ -0,0 +1,15 @@
+#Paint area calculator
+#Write your code below this line 👇
+def paint_calc(height, width, cover):
+    can_req = round((2*4)/5)
+    return can_req
+
+
+#Write your code above this line 👆
+# Define a function called paint_calc() so that the code below works.   
+
+# 🚨 Don't change the code below 👇
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
